af7a5088fef50e8081cecd2563acfa21e43f3bec
s.gadekar <sgadekar@calibo.com>
2024-06-20 09:58:57 +0530

    page title added

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,20 @@ var port = process.env.port || 3005;
 
 const server = createServer(
   (request: IncomingMessage, response: ServerResponse) => {
-    response.end(
-      "Hello, Welcome to Engineering Lab! Start editing to see some magic happen :)"
-    );
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+       if (err) {
+                response.writeHead(500, {'Content-Type': 'text/plain'});
+                response.end('Internal Server Error');
+                } else {
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                const pageTitle = 'Node.js 20 with TypeScript without Express';
+                const pageBody = "Hello, Welcome to Engineering Lab! Start editing to see some magic happen :)";
+                let html = data.toString();
+                html = html.replace('<title></title>', `<title>${pageTitle}</title>`);
+                html = html.replace('<p></p>', `<p>${pageBody}</p>`);
+                response.end(html);
+                }
+                });
   }
 );
 
